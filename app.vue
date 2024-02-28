31d496d7f39a4dbbaa1e7998fa8270086beac9ee
Dhanuvagman0 <161515197+Dhanuvagman0@users.noreply.github.com>
2024-02-28 14:05:07 +0530

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
-<html>
-  <head>hello</head>
-  <body>
-    <p>hello world</p>
-    </body>
-    </html>
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
+<template>
+  <div class="hello">
+    Hello Nuxt {{ version }}!
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
